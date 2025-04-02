b256daf9e1e2e7012709c7390d07286e3122130c
areejosama <106487998+areejosama@users.noreply.github.com>
2025-04-02 21:22:46 +0200

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async ()=>{
-    return await mongoose.connect(process.env.DBURL)
+    return await mongoose.connect(mongodb+srv://areejosama33:bYJ69KEE3vbvGLuy@cluster0.x4srn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
     .then(res=>{
         console.log('connected')
     }).catch(err=>{
