bb18ca7ecc07aea84f4fecdd31500841dda7933e
areejosama <areejosama33@gmail.com>
2025-04-02 20:45:52 +0200

    Changes in Base URL

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello, Express with ES6 is working!');
 });
-app.use(`${process.env.BASEURL}sector`, routes.sectorRouter);
-app.use(`${process.env.BASEURL}auth`, routes.authRouter);
-app.use(`${process.env.BASEURL}company`, routes.companyRouter);
-app.use(`${process.env.BASEURL}mainclass`, routes.classRouter);
+app.use(`/deepmetrics/api/v1/sector`, routes.sectorRouter);
+app.use(`/deepmetrics/api/v1/auth`, routes.authRouter);
+app.use(`/deepmetrics/api/v1/company`, routes.companyRouter);
+app.use(`/deepmetrics/api/v1/mainclass`, routes.classRouter);
 
 app.use('*',(req,res)=>{res.status(404).json({message:'Page Not Found'})});
 app.listen(port, () => {
